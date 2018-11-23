9521eff8f28afaa4766d0b18bc80de0fba29acbd
liuwei <liuwei@flksec.com>
2018-11-23 17:57:04 +0800

    初始化提交

---
+++ zookeepertest/src/main/java/com/example/zookeepertest/ZookeeperTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.zookeepertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperTestApplication.class, args);
+    }
+}
